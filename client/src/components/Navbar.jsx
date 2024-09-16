25b18e7ae5ec7324479f766d18509afadce51e9f
nilesh <nileshk262000@gmail.com>
2024-09-16 22:47:26 +0530

    Create nav

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-[#f4941c] py-2 h-[7vh]'>
+        <span className='text-5xl ml-8 text-white font-medium '>MoneyMap</span>
+    </div>
+  )
+}
+
+export default Navbar
