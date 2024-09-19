18aaed6168fbd86a4efadc8512566c557a1a9643
nilesh <nileshk262000@gmail.com>
2024-09-19 22:47:56 +0530

    Create transaction card

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,27 +1,47 @@
 import React, { useState } from 'react'
+import { BsPencilSquare } from "react-icons/bs";
 
+const TransactionCard = ({transaction})=>{
+    return(
+        <div className='bg-white h-1/2 w-1/5 shadow-lg m-2 py-4 px-2 hover:shadow-2xl'>
+            <div className={`w-full ${transaction.type === "Income" ? "bg-green-500" : "bg-red-500"} h-3`}></div>
+            <h1 className='text-2xl w-full font-semibold'>&#8377; {transaction.amount}</h1>
+            <h2 className='text-xl text-right'>{transaction.category}</h2>
+            <button className='text-[#7C00FE] text-3xl font-semibold p-1'><BsPencilSquare /></button>
+        </div>
+    )
+}
 const Transactions = () => {
-    const [transactions, setTransactions] = useState([])
-    useEffect(() => {
-        
-    }, [])
+    const [transactions, setTransactions] = useState([
+        {
+            id: 1,
+            date: "2022-01-01",
+            type: "Income",
+            amount: 100000,
+            category: "Salary"
+        },
+        {
+            id: 2,
+            date: "2022-01-02",
+            type: "Expense",
+            amount: 50000,
+            category: "Groceries"
+        },
+        {
+            id: 3,
+            date: "2022-01-03",
+            type: "Income",
+            amount: 200000,
+            category: "Savings"
+        }
+    ])
+    
   return (
-    <div>
-        <table className='table-auto'>
-            <thead>
-                <tr>
-                    <th className='px-3 py-4'>Date</th>
-                    <th className='px-3 py-4'>Type</th>
-                    <th className='px-3 py-4'>Amount</th>
-                    <th className='px-3 py-4'>Category</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    
-                </tr>
-            </tbody>
-        </table>
+    <div className='ml-3'>
+        <h1 className='text-3xl'>Transactions</h1>
+        {transactions.map((transaction)=>{
+            return <TransactionCard key={transaction.id} transaction={transaction}/>
+        })}
     </div>
   )
 }
