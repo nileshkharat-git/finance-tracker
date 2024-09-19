21e36009f432a06cfbbdc320ebf2c2747cdae40c
nilesh <nileshk262000@gmail.com>
2024-09-19 22:48:45 +0530

    Add Transactions coponent

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
+import AddIncome from '../components/AddIncome'
+import Transactions from '../components/Transactions'
 
 const Dashboard = () => {
   return (
-    <div className='h-[93vh]'>
+    <div className='h-[93vh] flex'>
         <section className='w-1/6 border-r-2 border-gray-100 h-full'>
             <ul className='text-2xl'>
-                <li className='text-center py-4 bg-gray-100 hover:bg-gray-200 cursor-pointer mb-1'>Income Sources</li>
-                <li className='text-center py-4 bg-gray-100 hover:bg-gray-200 cursor-pointer mb-1'>Expences</li>
-                <li className='text-center py-4 bg-gray-100 hover:bg-gray-200 cursor-pointer mb-1'>Statistics</li>
+                <li className='px-3 py-4 bg-gray-100 hover:bg-gray-200 cursor-pointer mb-1'>Transactions</li>
+                <li className='px-3 py-4 bg-gray-100 hover:bg-gray-200 cursor-pointer mb-1'>Income Sources</li>
+                <li className='px-3 py-4 bg-gray-100 hover:bg-gray-200 cursor-pointer mb-1'>Expences</li>
+                <li className='px-3 py-4 bg-gray-100 hover:bg-gray-200 cursor-pointer mb-1'>Statistics</li>
             </ul>
         </section>
+        <main className='w-5/6'>
+            <Transactions />
+        </main>
     </div>
   )
 }
