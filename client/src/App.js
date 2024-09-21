646cc641494f05beaf3ed2053c0508a0645235cb
nilesh <nileshk262000@gmail.com>
2024-09-21 23:29:31 +0530

    create setToken and getToken function to handle a token

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,32 @@
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import Dashboard from './pages/Dashboard';
 import Login from './components/Login';
 
+
+const setToken = (userToken)=>{
+  sessionStorage.setItem("token", JSON.stringify(userToken))
+}
+
+const getToken = ()=>{
+  const tokenString = sessionStorage.getItem("token")
+  const userToken = JSON.parse(tokenString)
+  return userToken?.token
+}
 function App() {
+    const token = getToken()
+    if(!token){
+      return <>
+              <Navbar/>
+              <Login setToken={setToken}/>
+             </>
+    }
   return (
-    <div>
-      <Login />
-    </div>
+    <>
+      <Navbar/>
+      <Dashboard/>
+    </>
   );
 }
 
