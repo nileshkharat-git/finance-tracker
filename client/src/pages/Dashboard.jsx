5eba402f00bd7a2e4357edd27e392c800a5fff2b
nilesh <nileshk262000@gmail.com>
2024-09-21 23:27:57 +0530

    Add self invoking function for switch between components

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,24 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 import AddIncome from '../components/AddIncome'
 import Transactions from '../components/Transactions'
+import IncomeList from '../components/IncomeList'
 
 
-const MainComponent = (Component=Transactions) => {  
-  return <Component />
-}
 const Dashboard = () => {
+  const [showComponent, setShowComponent] = useState('')
   return (
     <div className='h-[93vh] flex'>
         <section className='w-1/6 border-r-2 border-gray-100 h-full'>
             <ul className='text-2xl'>
-                <li className='px-3 py-4 bg-gray-100 hover:bg-gray-200 cursor-pointer mb-1' onClick={()=>{}}>Transactions</li>
-                <li className='px-3 py-4 bg-gray-100 hover:bg-gray-200 cursor-pointer mb-1' onClick={()=>{}}>Income Sources</li>
+                <li className='px-3 py-4 bg-gray-100 hover:bg-gray-200 cursor-pointer mb-1' onClick={() => setShowComponent('Transactions')}>Transactions</li>
+                <li className='px-3 py-4 bg-gray-100 hover:bg-gray-200 cursor-pointer mb-1' onClick={() => setShowComponent('Income Sources')}>Income Sources</li>
                 <li className='px-3 py-4 bg-gray-100 hover:bg-gray-200 cursor-pointer mb-1'>Expences</li>
                 <li className='px-3 py-4 bg-gray-100 hover:bg-gray-200 cursor-pointer mb-1'>Statistics</li>
             </ul>
         </section>
         <main className='w-5/6'>
-          <MainComponent />
+            {(() => {
+              switch (showComponent) {
+                case 'Transactions':
+                  return <Transactions/>
+                
+                case 'Income Sources':
+                  return <>
+                          <AddIncome/>
+                          <IncomeList/>
+                         </>
+
+                default:
+                  return <Transactions/>
+              }
+            })()}
         </main>
     </div>
   )
