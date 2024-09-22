bbc66db707426451f90f04410c1aa488c94b1433
nilesh <nileshk262000@gmail.com>
2024-09-22 13:06:50 +0530

    create index file for components

---
+++ client/src/components/index.jsx
@@ -0,0 +1,4 @@
+export {default as Navbar} from './Navbar'
+export {default as Login} from './Login'
+export {default as Transactions} from './Transactions'
+export {default as Income} from './Income'
