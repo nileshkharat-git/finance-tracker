44e70a25d72402623b314462fb1e9b2ef50b0a6f
nilesh <nileshk262000@gmail.com>
2024-09-23 23:21:29 +0530

    Fetch and list all transactions of the user

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { BsPencilSquare } from "react-icons/bs";
+import axios from 'axios';
+import useToken from '../hooks/useToken';
 
 const TransactionCard = ({transaction})=>{
     return(
-        <div className='bg-white h-1/2 w-1/5 shadow-lg m-2 py-4 px-2 hover:shadow-2xl'>
-            <div className={`w-full ${transaction.type === "Income" ? "bg-green-500" : "bg-red-500"} h-3`}></div>
+        <div className={`bg-white h-1/2 w-1/5 shadow-md mx-2 my-4 py-4 px-2 border-t-4 hover:shadow-xl ${transaction.transaction_type === "income" ? "border-green-500" : "border-red-500"}`}>
             <h1 className='text-2xl w-full font-semibold'>&#8377; {transaction.amount}</h1>
             <h2 className='text-xl text-right'>{transaction.category}</h2>
             <button className='text-[#7C00FE] text-3xl font-semibold p-1'><BsPencilSquare /></button>
@@ -12,29 +13,23 @@ const TransactionCard = ({transaction})=>{
     )
 }
 const Transactions = () => {
-    const [transactions, setTransactions] = useState([
-        {
-            id: 1,
-            date: "2022-01-01",
-            type: "Income",
-            amount: 100000,
-            category: "Salary"
-        },
-        {
-            id: 2,
-            date: "2022-01-02",
-            type: "Expense",
-            amount: 50000,
-            category: "Groceries"
-        },
-        {
-            id: 3,
-            date: "2022-01-03",
-            type: "Income",
-            amount: 200000,
-            category: "Savings"
-        }
-    ])
+    const {token} = useToken()
+    useEffect(() => {
+        const options = {
+            method: 'GET',
+            url: 'http://127.0.0.1:8000/api/transactions',
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          };
+          
+          axios.request(options).then(function (response) {
+            setTransactions(response.data)
+          }).catch(function (error) {
+            console.error(error);
+          });
+    }, [token])
+    const [transactions, setTransactions] = useState([])
     
   return (
     <div className='ml-3'>
@@ -42,6 +37,7 @@ const Transactions = () => {
         {transactions.map((transaction)=>{
             return <TransactionCard key={transaction.id} transaction={transaction}/>
         })}
+        
     </div>
   )
 }
