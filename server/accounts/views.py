1d7944c9afab1432183a0ce4e405be204aa24f28
nilesh <nileshk262000@gmail.com>
2024-10-11 22:52:19 +0530

    refactor:remove unwanted imports

--- server/accounts/views.py
+++ server/accounts/views.py
@@ -1,11 +1,8 @@
-from rest_framework import status, generics
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
 from rest_framework.permissions import AllowAny
-from django.contrib.auth import login, logout
 
 from .models import CustomUser
-from .serializers import CustomUserCreateSerializer, LoginSerializer
+from .serializers import CustomUserCreateSerializer
 
 class RegisterView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
