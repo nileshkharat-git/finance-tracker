a51872b6d8adcb86a11d3a5cc714ae8d187e1f49
Nilesh Kharat <nileshkharat.dev@gmail.com>
2025-07-30 09:42:41 +0530

    feat:config file

---
+++ client/src/config.js
@@ -0,0 +1,10 @@
+const config ={
+    API_BASE_URL:"http://127.0.0.1:8000",
+    APP_NAME:"Finance Tracker",
+    VERSION:"1.0.0",
+    FEATURE_TOGGLE:{
+        enableDarkMode:false
+    }
+}
+
+export default config
